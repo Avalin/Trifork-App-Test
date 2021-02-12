f84bc5050d1f9dc1b5fe8119fbbfb05be697767d
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2021-02-12 17:19:30 +0100

    -

---
+++ Assets/Scripts/Actions/RandomSpriteSwapAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomSpriteSwapAction : MonoBehaviour, IPressable
+{
+    [SerializeField] Image selectedImage;
+    [Range(0, 100)]
+    [SerializeField] int maxSprites;
+    [SerializeField] bool loadShroomSprites;
+
+    Sprite[] sprites;
+
+    void Start()
+    {
+        LoadSprites();
+    }
+
+    public void SelectRandomShroomSprite() 
+    {
+        var newSprite = sprites[Random.Range(0, maxSprites)];
+        if (selectedImage.sprite == newSprite) SelectRandomShroomSprite();
+        else selectedImage.sprite = newSprite;
+    }
+
+    public void Press()
+    {
+        SelectRandomShroomSprite();
+    }
+
+    private void LoadSprites() 
+    {
+        if(loadShroomSprites) sprites = Resources.LoadAll<Sprite>("Graphics/shrooms");
+    }
+}
---
+++ Assets/Scripts/Actions/SpriteJumpAction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SpriteJumpAction : MonoBehaviour, IPressable
+{
+    [SerializeField] Animator acSprite;
+    public void Press()
+    {
+        acSprite.Play("Jump");
+    }
+}
