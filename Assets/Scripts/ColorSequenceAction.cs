02a5efd86e7a974897281f0b9c84107bed9acce0
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2021-02-12 11:31:20 +0100

    Color Sequence

---
+++ Assets/Scripts/ColorSequenceAction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorSequenceAction : MonoBehaviour, IPressable
+{
+    //The image in which the color sequence action will affect
+    [SerializeField] Image selectedImage;
+
+    int pressCounter = 0;
+    Color selectedImageStartColor;
+    bool isChangingColor = false;
+    bool isResetAllowed = false;
+
+    public void Press()
+    {
+        pressCounter++;
+        StartCoroutine(ColorSequence());
+        StartCoroutine(ResetButton());
+    }
+
+    void Start()
+    {
+        if (selectedImage)
+            selectedImageStartColor = selectedImage.color;
+        else throw new Exception("Insert an image in the inspector for ColorSequenceAction of object " + gameObject.name);
+    }
+
+    IEnumerator ColorSequence() 
+    {
+        //Ensure only one ColorSequence coroutine is running
+        if (isChangingColor) yield break;
+        isChangingColor = true;
+
+        yield return new WaitForSeconds(2);
+        switch (pressCounter)
+        {
+            case 1:
+                selectedImage.color = Color.blue;
+                break;
+            case 2:
+                selectedImage.color = Color.red;
+                break;
+            case 5:
+                selectedImage.color = new Color(1, 0, 1, 1);
+                break;
+            default:
+                //Reset();
+                break;
+        }
+        pressCounter = 0;
+        isChangingColor = false;
+        isResetAllowed = true;
+    }
+
+    IEnumerator ResetButton() 
+    {
+        yield return new WaitUntil(() => isResetAllowed);
+        int currentPressCount = pressCounter;
+
+        yield return new WaitForSeconds(5);
+        if (currentPressCount == pressCounter && isResetAllowed)
+        {
+            Reset();
+        }
+        isResetAllowed = false;
+        StartCoroutine(ResetButton());
+    }
+
+    void Reset() 
+    {
+        pressCounter = 0;
+        selectedImage.color = selectedImageStartColor;
+    }
+}
---
+++ Assets/Scripts/RandomColorAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomColorAction : MonoBehaviour, IPressable
+{
+    //The image in which the random color action will affect
+    [SerializeField] Image selectedImage;
+
+    public void Press()
+    {
+        Color randomColor = Random.ColorHSV(0f, 1f, 0f, 1f, 1f, 1f);
+        selectedImage.color = randomColor;
+    }
+}
